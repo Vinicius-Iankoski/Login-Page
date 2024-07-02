65c4babdab5dbb044aec58a98d360381e1a22e81
Vinicius-Iankoski <vine.iankoski@gmail.com>
2024-07-02 14:48:00 -0300

    conexao com db

--- conexao.php
+++ conexao.php
@@ -1,3 +1,18 @@
 <?php
+    $dbHost = 'localhost';
+    $dbUser = 'root';
+    $dbSenha = '';
+    $dbName = 'login-page';
 
+    $conexao = new mysqli($dbHost, $dbUser, $dbSenha, $dbName);
+
+/* Teste
+
+     if ($conexao->connect_errno) {
+         echo "erro";
+     } else {
+         echo "conexao efetuada com sucesso";
+     }
+         
+*/
 ?>
