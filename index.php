3c0ef2cb924bced979d9dc32cb5ec6d9dc169a7f
Vinicius-Iankoski <vine.iankoski@gmail.com>
2024-07-02 14:55:33 -0300

    primeira parte do formulario

--- index.php
+++ index.php
@@ -10,6 +10,22 @@
     <title>Login</title>
 </head>
 <body>
-    
+    <form action="index.php" method="post">
+        <legend><b>Cadastro de dados</b></legend>
+        <br>
+        <input type="text" name="PrimeiroNome" required>
+        <label for="">Primeiro Nome</label>
+        <br>
+        <input type="text" name="SegundoNome" required>
+        <label for="">Segundo nome</label>
+        <br>
+        <input type="email" name="Email" required>
+        <label for="">Email</label>
+        <br>
+        <input type="password" name="Password" required>
+        <label for="">Senha</label>
+        <br>
+        <input type="submit" name="Submit">
+    </form>
 </body>
 </html>
